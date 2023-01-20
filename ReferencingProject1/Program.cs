9131d92b74bd88b55c3a8ebd005fc12349275e7a
lilia shamsutdinova <lilia.shamsutdinova@jetbrains.com>
2023-01-20 21:08:55 +0100

    Add usages.

--- ReferencingProject1/Program.cs
+++ ReferencingProject1/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using ReferencedProject;
+
 Console.WriteLine("Hello, World!");
+
+Class1 class1 = new Class1();
+// Class1
+
+var class1_local = "Class1";
