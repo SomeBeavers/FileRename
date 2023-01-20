566302b9e00c65eaf0995c42ee0519ff62b2fab3
lilia shamsutdinova <lilia.shamsutdinova@jetbrains.com>
2023-01-20 20:58:09 +0100

    Add usages of Multi1

---
+++ CoreApp/UseMulti1.cs
@@ -0,0 +1,12 @@
+﻿using Multi;
+
+namespace CoreApp;
+
+public class UseMulti1
+{
+    // Comment: Multi1
+    public void Test(Multi1 multi1)
+    {
+        var stringLiteral = "Multi1";
+    }
+}
---
+++ net6_App/UseMulti1.cs
@@ -0,0 +1,12 @@
+﻿using Multi;
+
+namespace net6_App;
+
+public class UseMulti1
+{
+    // Comment: Multi1
+    public void Test(Multi1 multi1)
+    {
+        var stringLiteral = "Multi1";
+    }
+}
