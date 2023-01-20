c9c84379ad3ac6a84cca9ded944a9bcf6027a7ee
lilia shamsutdinova <lilia.shamsutdinova@jetbrains.com>
2023-01-20 20:08:57 +0100

    Update

---
+++ FrameworkApp/Class1_Renamed.cs
@@ -0,0 +1,7 @@
+﻿namespace FrameworkApp
+{
+    public class Class1_Renamed
+    {
+        
+    }
+}
---
+++ FrameworkApp/Use_Class1_Renamed.cs
@@ -0,0 +1,10 @@
+﻿namespace FrameworkApp
+{
+    public class Use_Class1_Renamed
+    {
+        public void Test(Class1_Renamed class1_renamed)
+        {
+            // Class1_Renamed
+        }
+    }
+}
