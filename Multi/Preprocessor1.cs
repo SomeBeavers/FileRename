f8e56261cfbd2fac3e00b59d420da66d521481bf
lilia shamsutdinova <lilia.shamsutdinova@jetbrains.com>
2023-01-21 12:40:17 +0100

    Add preprocessor.

---
+++ Multi/Preprocessor1.cs
@@ -0,0 +1,17 @@
+﻿namespace Multi;
+
+public class Preprocessor1
+{
+#if NET6_0
+    public Preprocessor1()
+    {
+    }
+    #else
+    public string Name { get; set; }
+        public Preprocessor1(string name)
+    {
+        Name = name;    
+    }
+#endif
+
+}
