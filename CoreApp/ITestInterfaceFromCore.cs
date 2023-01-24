155ab9948700310004fc65583922a522113c8c17
lilia shamsutdinova <lilia.shamsutdinova@jetbrains.com>
2023-01-24 14:57:34 +0100

    Upd core project.

---
+++ CoreApp/ITestInterfaceFromCore.cs
@@ -0,0 +1,41 @@
+﻿namespace CoreApp
+{
+    // I am ITestInterfaceFromCore interface.
+    public interface ITestInterfaceFromCore
+    {
+        void Test();
+    }
+
+    /// <summary>
+    /// TestInterfaceFromCore3 derives from ITestInterfaceFromCore
+    /// </summary>
+    class TestInterfaceFromCore3 : ITestInterfaceFromCore
+    {
+        public void Test()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    /// <summary>
+    /// TestInterfaceFromCore2 derives from ITestInterfaceFromCore
+    /// </summary>
+    class TestInterfaceFromCore2 : ITestInterfaceFromCore
+    {
+        public void Test()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    /// <summary>
+    /// TestInterfaceFromCore derives from ITestInterfaceFromCore
+    /// </summary>
+    class TestInterfaceFromCore : ITestInterfaceFromCore
+    {
+        public void Test()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
