c55dfa988264c5402de0fbd3f00d671dc3ea27a6
lilia shamsutdinova <lilia.shamsutdinova@jetbrains.com>
2023-01-24 18:20:27 +0100

    upd

---
+++ FrameworkApp/TypesInFile/FileWithInterface.cs
@@ -0,0 +1,21 @@
+﻿#region header region
+
+// FileRename
+// FileWithInterface.cs
+
+#endregion
+
+namespace ManyClassesGenerated;
+
+public abstract class FileWithInterface
+{
+    public abstract void Test();
+}
+
+public class UseFileWithInterface
+{
+    public void Test(FileWithInterface fileWithInterface)
+    {
+        var t = 0;
+    }
+}
