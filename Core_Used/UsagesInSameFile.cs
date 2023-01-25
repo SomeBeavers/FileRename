9d20206ad40717e693330104f62bff6f16be3e78
lilia shamsutdinova <lilia.shamsutdinova@jetbrains.com>
2023-01-25 11:09:39 +0100

    Add projects to test usages.

---
+++ Core_Used/UsagesInSameFile.cs
@@ -0,0 +1,37 @@
+﻿namespace Core_Used;
+
+/// <summary>
+///     <see cref="UsagesInSameFile" /> is UsagesInSameFile file.
+/// </summary>
+public class UsagesInSameFile
+{
+    // this is the c-or for UsagesInSameFile
+}
+
+internal class UsagesInSameFileImpl : UsagesInSameFile
+{
+    /// <summary>
+    ///     This is a child of <see cref="UsagesInSameFile" /> named UsagesInSameFileImpl in
+    ///     UsagesInSameFile.cs -_-
+    /// </summary>
+    public UsagesInSameFileImpl()
+    {
+    }
+
+    public UsagesInSameFile UsagesInSameFile { get; set; }
+
+    private static void TestUsagesInSameFile(UsagesInSameFile usagesInSameFile)
+    {
+        if (usagesInSameFile == null)
+        {
+            var s =
+                $"""
+                {usagesInSameFile}
+                """;
+        }
+    }
+}
+
+internal class UsagesInSameFileImplImpl : UsagesInSameFileImpl
+{
+}
